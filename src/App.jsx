6dfbd084ee3746f9c614b025e24ca843ce093d26
priyanshu buley <buley2607@gmail.com>
2023-12-01 13:13:34 +0530

    intial commit

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import './App.css';
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import Home from './components/Home';
+import About from './components/About';
+import Contact from './components/Contact';
+
+function App() {
+  return (
+    <div className="App">
+      <div>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Home() {
+  return (
+    <div>
+      <h1>this is my Home Page</h1>
+    </div>
+  );
+}
+
+export default Home;

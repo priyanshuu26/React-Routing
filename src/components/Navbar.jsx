eda24e7cae3969374a2d10cce19149f9d194d635
priyanshu buley <buley2607@gmail.com>
2023-12-01 13:41:51 +0530

    modify with navlinks

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+
+function Navbar() {
+  return (
+    <div>
+      <nav>
+        <NavLink to="/">Home</NavLink>
+        <NavLink to="/about">about</NavLink>
+        <NavLink to="/contact">contact</NavLink>
+      </nav>
+    </div>
+  );
+}
+
+export default Navbar;

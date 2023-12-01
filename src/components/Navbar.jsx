8aaf00c68f41ffed85be4a354819204b76f2f448
priyanshu buley <buley2607@gmail.com>
2023-12-01 13:31:41 +0530

    adding class

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 function Navbar() {
   return (
     <div>
-      <nav>
+      <nav className="nav">
         <Link to="/">Home</Link>
         <Link to="/about">about</Link>
         <Link to="/contact">contact</Link>

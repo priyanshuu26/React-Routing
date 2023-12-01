551c46464881abfb2d4bb4585595aeeca278d449
priyanshu buley <buley2607@gmail.com>
2023-12-01 14:10:49 +0530

    intial commit

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,9 +1,22 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 
 function Home() {
+  const navigate = useNavigate();
+  const NavigateTo = () => {
+    const name = 'priyanshu';
+    if (name === 'priyanshu') {
+      navigate('/contact');
+    } else {
+      navigate('/about');
+    }
+  };
   return (
     <div>
       <h1>this is my Home Page</h1>
+      <button type="button" onClick={() => NavigateTo()}>
+        contact
+      </button>
     </div>
   );
 }

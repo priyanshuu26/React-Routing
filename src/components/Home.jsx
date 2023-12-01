6a42955e5e5d4389152ef24742685c932e3eca64
priyanshu buley <buley2607@gmail.com>
2023-12-01 13:54:15 +0530

    initial commit

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 
 function Home() {
+  const navigateTo = useNavigate();
   return (
     <div>
       <h1>this is my Home Page</h1>
+      <button type="button" onClick={() => navigateTo('/contact')}>
+        Contact
+      </button>
     </div>
   );
 }

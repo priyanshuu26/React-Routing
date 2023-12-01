c830fb57db4ca4926682568a36baddb0bd63987d
priyanshu buley <buley2607@gmail.com>
2023-12-01 13:42:24 +0530

    adding component

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,13 @@ import { Routes, Route } from 'react-router-dom';
 import Home from './components/Home';
 import About from './components/About';
 import Contact from './components/Contact';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
     <div className="App">
       <div>
+        <Navbar />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
